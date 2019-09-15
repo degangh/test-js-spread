bedadcd2ac475c1c0f59774f9b3e9b97de5fa7ea
nicolas <degang.huang@gmail.com>
2019-09-15 22:28:17 +0930

    fetch data from an API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <h2>{{title}}</h2>
+    <button @click="fetchCurrencyData">Fetch Currencty Data</button>
+    <section-one></section-one>
+    <hr>
+    <section-two></section-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
+import SectionOne from './components/SectionOne'
+import SectionTwo from './components/SectionTwo'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    SectionOne,
+    SectionTwo
+  },
+  data() {
+    return {
+      title: 'Test Javascript Spread'
+    }
+  },
+
+  mounted() {
+    console.log('oops')
+  },
+
+  methods: {
+    fetchCurrencyData() {
+      fetch('https://api.exchangeratesapi.io/latest?base=AUD&symbols=CNY')
+      .then((response)=> {return response.json()})
+      .then((data)=>{console.log(data)})
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
